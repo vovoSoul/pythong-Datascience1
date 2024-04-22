1baf89912447b53f7fc71451b7f5c2290b7ba734
vovoSoul <164639325+vovoSoul@users.noreply.github.com>
2024-04-22 13:56:23 -0400

    using sample data created layout for graph

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import csv
 #for better visuals in the terminal
 from rich import print
 
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 #extracting the info from csv with DictReader
 filename= 'data.csv'
@@ -18,8 +18,16 @@ with open (filename, 'r') as csvfile:
         print(year)
         print(population)
         print(continent)
-
-
-
-
         
+    
+plt.plot([2001,2002,2003, 2004], [20, 40, 60, 90], label="africa", marker="o")
+plt.plot([2001, 2002, 2003, 2004], [20, 50, 100, 200], label="asia", marker="o")
+plt.legend()
+plt.tight_layout()
+plt.xlabel("Years")
+plt.ylabel("Population")
+plt.title("Number of Internet User per Continent through years")
+plt.grid(True)
+
+plt.show()
+    
